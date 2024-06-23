439217c8374e29666c27c6851058a2ff89d74aeb
Ruslan Kalendar <78969155+rkalendar@users.noreply.github.com>
2024-06-23 20:35:03 +0500

    Add files via upload

--- src/SequencesClustering.java
+++ src/SequencesClustering.java
@@ -1,3 +1,4 @@
+
 import java.util.HashMap;
 
 public final class SequencesClustering {
@@ -29,10 +30,10 @@ public final class SequencesClustering {
         int n = 0;
         String s;
         String a;
-        cx = new int[nseq];      // cluster
         int[] lx = new int[nseq];
-
         HashMap<String, int[]> map = new HashMap<>();
+        //  Map<String, int[]> m = new TreeMap<>();
+        HashMap<String, int[]> m = new HashMap<>();
 
         for (int j = 0; j < nseq; j++) {
             lx[j] = seq[j].length();
@@ -40,55 +41,122 @@ public final class SequencesClustering {
                 a = dna.ComplementDNA(seq[j]);
                 for (int i = kmer - 1; i < lx[j]; i++) {
                     s = seq[j].substring(i - kmer + 1, i + 1);
-                    if (map.containsKey(s)) {
-                        int[] t = map.get(s);
-                        t[j]++;
-                        map.put(s, t);
+                    if (m.containsKey(s)) {
+                        int[] t = m.get(s);
+                        if (t[1] < (j + 1)) {
+                            t[0]++;       // number of seq[] 
+                            t[1] = j + 1; // index seq[]+1 
+                            m.replace(s, t);
+                        }
                     } else {
-                        int[] t = new int[nseq];
-                        t[j] = 1;
-                        map.put(s, t);
+                        int[] t = new int[2];
+                        t[0] = 1;
+                        t[1] = j + 1;
+                        m.put(s, t);
                     }
+
                     s = a.substring(i - kmer + 1, i + 1);
-                    if (map.containsKey(s)) {
-                        int[] t = map.get(s);
-                        t[j]++;
-                        map.put(s, t);
+                    if (m.containsKey(s)) {
+                        int[] t = m.get(s);
+                        if (t[1] < (j + 1)) {
+                            t[0]++;       // number of seq[] 
+                            t[1] = j + 1; // index seq[]+1 
+                            m.replace(s, t);
+                        }
                     } else {
-                        int[] t = new int[nseq];
-                        t[j] = 1;
-                        map.put(s, t);
+                        int[] t = new int[2];
+                        t[0] = 1;
+                        t[1] = j + 1;
+                        m.put(s, t);
                     }
                 }
             }
         }
+        for (int j = 0; j < nseq; j++) {
+            lx[j] = seq[j].length();
+            if (lx[j] > minlenseq) {
+                a = dna.ComplementDNA(seq[j]);
+                for (int i = kmer - 1; i < lx[j]; i++) {
+                    s = seq[j].substring(i - kmer + 1, i + 1);
+
+                    if (map.containsKey(s)) {
+                        int[] y = map.get(s);
+                        if (y[0] == j) {
+                            y[y[1]]++;
+                        } else {
+                            y[0] = j;
+                            y[y[1] + 1] = j;
+                            y[y[1] + 2] = 1;
+                        }
+                        map.replace(s, y);
+                    } else {
+                        int[] t = m.get(s);
+                        int[] y = new int[t[0] + t[0] + 2];
+                        y[0] = j; // last seq[]
+                        y[1] = 3; // last x[i]
+                        y[2] = j; // data 1= seq[]
+                        y[3] = 1; // data 2= n->kmer at seq[]
+                        map.put(s, y);
+                    }
+
+                    s = a.substring(i - kmer + 1, i + 1);
+                    if (map.containsKey(s)) {
+                        int[] y = map.get(s);
+                        if (y[0] == j) {
+                            y[y[1]]++;
+                        } else {
+                            y[0] = j;
+                            y[y[1] + 1] = j;
+                            y[y[1] + 2] = 1;
+                        }
+                        map.replace(s, y);
+                    } else {
+                        int[] t = m.get(s);
+                        int[] y = new int[t[0] + t[0] + 2];
+                        y[0] = j; // last seq[]
+                        y[1] = 3; // last x[i]
+                        y[2] = j; // data 1= seq[]
+                        y[3] = 1; // data 2= n->kmer at seq[]
+                        map.put(s, y);
+                    }
+                }
+            }
+        }
+        m = new HashMap<>();
 
         //clustering
+        cx = new int[nseq];      // clusters        
         for (int j = 0; j < nseq; j++) {
             if (lx[j] > minlenseq) {
                 int[] x = new int[nseq];
                 a = dna.ComplementDNA(seq[j]);
+
                 for (int i = kmer - 1; i < lx[j]; i++) {
                     s = seq[j].substring(i - kmer + 1, i + 1);
                     int[] t = map.get(s);
-                    for (int h = 0; h < nseq; h++) {
-                        if (t[h] > 0) {
-                            x[h]++;
+                    for (int h = 2; h < t.length; h = h + 2) {
+                        if (t[h] == j) {
+                            x[t[h]]++;
+                        } else {
+                            x[t[h]] = x[t[h]] + t[h + 1];
                         }
                     }
+
                     s = a.substring(i - kmer + 1, i + 1);
                     t = map.get(s);
-                    for (int h = 0; h < nseq; h++) {
-                        if (t[h] > 0) {
-                            x[h]++;
+                    for (int h = 2; h < t.length; h = h + 2) {
+                        if (t[h] == j) {
+                            x[t[h]]++;
+                        } else {
+                            x[t[h]] = x[t[h]] + t[h + 1];
                         }
                     }
                 }
 
                 for (int h = 0; h < nseq; h++) {
-                    if (x[h] > 0) {
-                        int m = (100 * x[h]) / x[j];
-                        if (m > sim) {
+                    if (x[h] > 0 && x[j] > 0) {
+                        int v = (100 * x[h]) / x[j];
+                        if (v > sim) {
                             if (cx[j] == 0) {
                                 if (cx[h] > 0) {
                                     cx[j] = cx[h];
@@ -107,9 +175,7 @@ public final class SequencesClustering {
                                 } else {
                                     cx[h] = cx[j];
                                 }
-
                             }
-
                         }
                     }
                 }
